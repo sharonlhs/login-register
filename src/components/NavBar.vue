5830576f27b079f6e1029d1149888504d19a2d7b
sharonlhs <horshunn@gmail.com>
2023-03-24 12:06:38 +0800

    fixed nav-bar toggle icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,24 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <RouterLink  class="nav-item nav-link" to="/">Home</RouterLink>
-      <RouterLink class="nav-item nav-link" to="/login">Login</RouterLink>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Home</RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarSupportedContent">
+        <div class="navbar-nav me-auto mb-2 mb-lg-0">
+          <RouterLink class="nav-item nav-link" to="/">Home</RouterLink>
+          <RouterLink class="nav-item nav-link" to="/login">Login</RouterLink>
+          <RouterLink class="nav-item nav-link" to="/signup">Sign Up</RouterLink>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
-  export default {
-    name: 'NavBar'
-  }
+export default {
+  name: 'NavBar'
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import routes from './router'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle"
 
 createApp(App).use(routes).mount('#app')
 
