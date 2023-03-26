e959d3a46644359df9e60465e9566ad5b181f154
sharonlhs <horshunn@gmail.com>
2023-03-26 14:29:48 +0800

    update: register-page w/ bootstrap v1.0

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="wrapper">
-    <h1>Register Now</h1>
-    <form class="form-group">
-      <label>Email address:</label>
-        <input class="input-field" v-model="email" placeholder="Enter email address"/>
-        <label>Password:</label>
-        <input class="input-field" v-model="password" placeholder="Enter password"/>
-        <label>Re-enter password:</label>
-        <input class="input-field" v-model="password" placeholder="Re-enter password"/>
-        <button class="cta">Register Now</button>
-        <div class="link">
-          <RouterLink class="nav-item nav-link" to="/login">Already a user? Login now</RouterLink>
-        </div>
+  <div class="container mb-3 p-5">
+    <form class="mx-auto col-10 col-md-8 col-lg-6" autocomplete="off">
+      <h1 class="mb-3">Register</h1>
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Re-enter password</label>
+      </div>
+      <div class="col-md-12 mt-3 text-center">
+        <button type="button" class="mt-3 mb-3 col-12 btn btn-primary">Register now</button>
+        <RouterLink class="link-primary" to="/login">Already a user. Login Now</RouterLink>
+      </div>
     </form>
+
   </div>
 </template>
 
 <script>
-
 </script>
