7bf11473a7cdb3efdf0043ad37f58333c18b4014
sharonlhs <horshunn@gmail.com>
2023-03-26 14:36:25 +0800

    update: forgot-password w/ bootstrap v1.0

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="wrapper">
-    <h1>Forgot Password?</h1>
-    <form class="form-group">
-      <label>Email address:</label>
-      <input class="input-field" v-model="email" placeholder="Enter email address" />
-      <label>Password:</label>
-      <input class="input-field" v-model="password" placeholder="Enter password" />
-      <label>Re-enter password:</label>
-      <input class="input-field" v-model="password" placeholder="Re-enter password" />
-      <button class="cta">Update Password</button>
-      <div class="link">
-        <RouterLink class="nav-item nav-link" to="/login">Login Now</RouterLink>
+  <div class="container mb-3 p-5">
+    <form class="mx-auto col-10 col-md-8 col-lg-6" autocomplete="off">
+      <h1 class="mb-3">Forgot Password</h1>
+      <div class="mb-3">
+        <label for="staticEmail">Email address</label>
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" placeholder="email@example.com">
+      </div>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">New password</label>
+      </div>
+      <div class="col-md-12 mt-3 text-center">
+        <button type="button" class="mt-3 mb-3 col-12 btn btn-primary">Update Password</button>
+        <RouterLink class="link-primary" to="/login">Login Now</RouterLink>
       </div>
     </form>
+
   </div>
 </template>
 
-<script setup>
-
+<script>
 </script>
