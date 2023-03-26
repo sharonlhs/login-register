dde4628096ea1ffc64c9e5a1dfff7644369c0b02
sharonlhs <horshunn@gmail.com>
2023-03-26 10:37:10 +0800

    update: login-page w/ bootstrap

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="wrapper">
     <h1>Login page</h1>
-    <form class="form-group">
-      <label>Email address:</label>
-      <input class="input-field" v-model="email" placeholder="Enter email address" />
-      <label>Password:</label>
-      <input class="input-field" v-model="password" placeholder="Enter password" />
-      <RouterLink class="link" to="/forgot-password">Forgot password?</RouterLink>
-      <button class="cta">Login</button>
-      <div class="link">
-        <RouterLink class="nav-item nav-link" to="/signup">Not a user? Register now</RouterLink>
+    <form>
+      <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+      <div class="d-grid gap-2">
+      <RouterLink class="link-primary float-end" to="/forgot-password">Forgot password?</RouterLink>
+      <button type="button" class="btn btn-primary">Login</button>
+      <RouterLink class="link-primary" to="/signup">I am new. Register now</RouterLink>
       </div>
     </form>
+
   </div>
 </template>
 
